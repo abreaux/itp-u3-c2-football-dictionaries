462b41a28a6bf45c27c594795f6f71f55ab11f11
abreaux <breaux.aimee@gmail.com>
2020-08-05 21:50:33 -0400

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,7 @@
 def players_by_position(squads_list):
-    pass
+    r_dict = {}
+    for player in squads_list:
+        r_dict[player[1]]= []
+    for player in squads_list:
+        r_dict[player[1]].append(player[2])
+    return r_dict
